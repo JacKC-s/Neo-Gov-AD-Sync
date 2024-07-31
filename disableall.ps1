d16008e42a3bf2ca6c9e85f628b1e553c8ee3255
Jack Costello <163896934+JacKC-s@users.noreply.github.com>
2024-07-31 13:11:03 -0400

    Update disableall.ps1 - Now Working

--- disableall.ps1
+++ disableall.ps1
@@ -1,14 +1,14 @@
 # Disabled account destination
-$destinationOU = "OU=Disabled Accounts,DC=Example,DC=Example,DC=Example,DC=org"
+$destinationOU = "OU=Disabled Accounts,DC=THTOV,DC=viennamail,DC=viennava,DC=gov"
 
 # Grabs users who were determined to be unneeded
 $removedUsers = Get-Content -Path .\"inconsistent_users.json" -Raw | ConvertFrom-Json
 
 # Iterates through list to move each user into the disabled user destination and disables the users
-for ($i = 0; $i -lt $removedUsers.Count; $i++) {
+foreach ($item in $removedUsers) {
 
     # Puts Last and First names into vars
-    $fullName = $removedUsers[$i]
+    $fullName = $item.Name
     $lastName, $firstName = $fullName -split ',\s+'
 
     $user = Get-ADUser -Filter "Name -like'*$($lastName)*$($firstName)*'"
